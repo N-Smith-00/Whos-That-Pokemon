eafc1e1708e97d12f2673f0fe5c6efee596441cb
Nicholas-Smith463 <Nicholas.Smith463@gmail.com>
2023-01-12 17:16:26 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+import discord
+from flask import Flask
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user.name}')
+
+@client.event
+async def on_message(message):
+    channel = message.channel
+    user = message.author
+
+    if message.content.startswith('!pstart'):
+        pass
+
+@app.route("/")
+def main():
+    client.run(os.getenv('TOKEN'))
+    return "Who' That Pokemon (WIP)"
